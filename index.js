f38b6830bf73287cf2632223f97d47dee0635e99
Kathryn <kathrynbruce7@gmail.com>
2023-03-21 10:04:46 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+let multiply = num1*num2;
+
+let random = (Math.floor(Math.random()*10)+1);
+const num3 = 14;
+let mod = num3%5;
+const num4=20;
+let max= (Math.max(20));
